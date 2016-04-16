c7076f84b117ef61aa42b6ce17cbdeee61ecdb3d
zxwtry <zxwtry@qq.com>
2016-04-16 23:37:56 +0800

    修改对Android的发送者

--- src/android/PorduceMessageForAndroid.java
+++ src/android/PorduceMessageForAndroid.java
@@ -1,7 +1,16 @@
 package android;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
 public class PorduceMessageForAndroid {
-	public static void main(String[] args) {
-		
+	private static final String IP = "127.0.0.1";
+	private static final String ACTIVE_MQ_TCP_URI = "tcp://"+IP+":61616";
+	public static void main(String[] args) throws Exception{
+		ConnectionFactory factory = new ActiveMQConnectionFactory(ACTIVE_MQ_TCP_URI);
+		Connection con = factory.createConnection();
+		con.start();
 	}
 }
